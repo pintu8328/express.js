88bbec596fdd3f761a28737d864a55f203fd95cd
pintu8328 <97545631+pintu8328@users.noreply.github.com>
2024-02-26 19:17:59 +0530

    Router and filters

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ const express= require('express')
 
 const app=express()
 
-const bodyParser= require('body-parser')
 
+
+const adminRouter= require('./routes/admin')
+const shopRouter= require('./routes/shop')
+
+
+
+const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extends:false}))
 
-app.use('/add-product',(req,res,next)=>{
-    res.send("<form method='POST' action='/product'><input type='text' name='title'></input><input type='text' name='size' placeholder='inter-size'></input><button type='submit'>submit</button></form>")
+app.use('/shop',shopRouter);
+app.use('/admin',adminRouter);
+app.use((req,res,next)=>{
+    res.send('<h1>Page Not Found</h1>')
 })
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>welcome to express</h1>')
-})
 
 app.listen(3000,()=>{
     console.log("Listening on port 3000")
