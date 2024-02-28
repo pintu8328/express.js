2c95d95fd490851634fe941fe10d21029a5044af
pintu8328 <97545631+pintu8328@users.noreply.github.com>
2024-02-28 23:16:20 +0530

    Models

--- routes/shop.js
+++ routes/shop.js
@@ -2,18 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const userController= require('../controllers/user')
-
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  });
-
-router.get('/contactus',userController.addContact );
-
-  router.post('/success', userController.postContact);
+router.get('/', productsController.getProducts);
 
 module.exports = router;
